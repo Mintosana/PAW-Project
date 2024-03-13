0a2d4b02e635307bee8f771c1183ecc89a54f168
Rece Cristian <rece.cristian@gmail.com>
2024-03-14 00:48:10 +0200

    uploaded project

---
+++ Proiect_Resurse_Umane/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace Proiect_Resurse_Umane
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ComboBox cbTipCurs;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNumePersoana = new System.Windows.Forms.TextBox();
+            this.tbVarstaPersoana = new System.Windows.Forms.TextBox();
+            this.tbFacultatePersoana = new System.Windows.Forms.TextBox();
+            this.tbDepartamentJob = new System.Windows.Forms.TextBox();
+            this.tbNumeJob = new System.Windows.Forms.TextBox();
+            this.tbNumeCurs = new System.Windows.Forms.TextBox();
+            this.tbSalariuJob = new System.Windows.Forms.TextBox();
+            this.lbNumePersoana = new System.Windows.Forms.Label();
+            this.lbVarstaPersoana = new System.Windows.Forms.Label();
+            this.lbFacultatePersoana = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnAdaugarePersoana = new System.Windows.Forms.Button();
+            this.btnAdaugareCurs = new System.Windows.Forms.Button();
+            this.btnAdaugareJob = new System.Windows.Forms.Button();
+            this.DataGrid = new System.Windows.Forms.DataGridView();
+            this.btnAfisarePersoane = new System.Windows.Forms.Button();
+            this.btnAfisareCursuri = new System.Windows.Forms.Button();
+            this.btnAfisareJoburi = new System.Windows.Forms.Button();
+            cbTipCurs = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbTipCurs
+            // 
+            cbTipCurs.FormattingEnabled = true;
+            cbTipCurs.Items.AddRange(new object[] {
+            "Compliance",
+            "Onboarding",
+            "Tehnical",
+            "Management"});
+            cbTipCurs.Location = new System.Drawing.Point(43, 266);
+            cbTipCurs.Name = "cbTipCurs";
+            cbTipCurs.Size = new System.Drawing.Size(100, 23);
+            cbTipCurs.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introducere Persoana";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Introducere Curs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Introducere Job";
+            // 
+            // tbNumePersoana
+            // 
+            this.tbNumePersoana.Location = new System.Drawing.Point(43, 49);
+            this.tbNumePersoana.Name = "tbNumePersoana";
+            this.tbNumePersoana.Size = new System.Drawing.Size(100, 23);
+            this.tbNumePersoana.TabIndex = 3;
+            // 
+            // tbVarstaPersoana
+            // 
+            this.tbVarstaPersoana.Location = new System.Drawing.Point(43, 78);
+            this.tbVarstaPersoana.Name = "tbVarstaPersoana";
+            this.tbVarstaPersoana.Size = new System.Drawing.Size(100, 23);
+            this.tbVarstaPersoana.TabIndex = 4;
+            // 
+            // tbFacultatePersoana
+            // 
+            this.tbFacultatePersoana.Location = new System.Drawing.Point(43, 107);
+            this.tbFacultatePersoana.Name = "tbFacultatePersoana";
+            this.tbFacultatePersoana.Size = new System.Drawing.Size(100, 23);
+            this.tbFacultatePersoana.TabIndex = 5;
+            // 
+            // tbDepartamentJob
+            // 
+            this.tbDepartamentJob.Location = new System.Drawing.Point(43, 432);
+            this.tbDepartamentJob.Name = "tbDepartamentJob";
+            this.tbDepartamentJob.Size = new System.Drawing.Size(100, 23);
+            this.tbDepartamentJob.TabIndex = 6;
+            // 
+            // tbNumeJob
+            // 
+            this.tbNumeJob.Location = new System.Drawing.Point(43, 403);
+            this.tbNumeJob.Name = "tbNumeJob";
+            this.tbNumeJob.Size = new System.Drawing.Size(100, 23);
+            this.tbNumeJob.TabIndex = 7;
+            // 
+            // tbNumeCurs
+            // 
+            this.tbNumeCurs.Location = new System.Drawing.Point(43, 234);
+            this.tbNumeCurs.Name = "tbNumeCurs";
+            this.tbNumeCurs.Size = new System.Drawing.Size(100, 23);
+            this.tbNumeCurs.TabIndex = 8;
+            // 
+            // tbSalariuJob
+            // 
+            this.tbSalariuJob.Location = new System.Drawing.Point(43, 461);
+            this.tbSalariuJob.Name = "tbSalariuJob";
+            this.tbSalariuJob.Size = new System.Drawing.Size(100, 23);
+            this.tbSalariuJob.TabIndex = 10;
+            // 
+            // lbNumePersoana
+            // 
+            this.lbNumePersoana.AutoSize = true;
+            this.lbNumePersoana.Location = new System.Drawing.Point(163, 52);
+            this.lbNumePersoana.Name = "lbNumePersoana";
+            this.lbNumePersoana.Size = new System.Drawing.Size(40, 15);
+            this.lbNumePersoana.TabIndex = 11;
+            this.lbNumePersoana.Text = "Nume";
+            // 
+            // lbVarstaPersoana
+            // 
+            this.lbVarstaPersoana.AutoSize = true;
+            this.lbVarstaPersoana.Location = new System.Drawing.Point(163, 81);
+            this.lbVarstaPersoana.Name = "lbVarstaPersoana";
+            this.lbVarstaPersoana.Size = new System.Drawing.Size(38, 15);
+            this.lbVarstaPersoana.TabIndex = 12;
+            this.lbVarstaPersoana.Text = "Varsta";
+            // 
+            // lbFacultatePersoana
+            // 
+            this.lbFacultatePersoana.AutoSize = true;
+            this.lbFacultatePersoana.Location = new System.Drawing.Point(163, 110);
+            this.lbFacultatePersoana.Name = "lbFacultatePersoana";
+            this.lbFacultatePersoana.Size = new System.Drawing.Size(55, 15);
+            this.lbFacultatePersoana.TabIndex = 13;
+            this.lbFacultatePersoana.Text = "Facultate";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(161, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Nume";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(161, 266);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Tip";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(163, 461);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 15);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Salariu";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(163, 408);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Nume";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(163, 432);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(76, 15);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Departament";
+            // 
+            // btnAdaugarePersoana
+            // 
+            this.btnAdaugarePersoana.Location = new System.Drawing.Point(43, 148);
+            this.btnAdaugarePersoana.Name = "btnAdaugarePersoana";
+            this.btnAdaugarePersoana.Size = new System.Drawing.Size(141, 21);
+            this.btnAdaugarePersoana.TabIndex = 19;
+            this.btnAdaugarePersoana.Text = "Introduceti Persoana";
+            this.btnAdaugarePersoana.UseVisualStyleBackColor = true;
+            this.btnAdaugarePersoana.Click += new System.EventHandler(this.btnAdaugarePersoana_Click);
+            // 
+            // btnAdaugareCurs
+            // 
+            this.btnAdaugareCurs.Location = new System.Drawing.Point(43, 312);
+            this.btnAdaugareCurs.Name = "btnAdaugareCurs";
+            this.btnAdaugareCurs.Size = new System.Drawing.Size(141, 21);
+            this.btnAdaugareCurs.TabIndex = 20;
+            this.btnAdaugareCurs.Text = "Introduceti Curs";
+            this.btnAdaugareCurs.UseVisualStyleBackColor = true;
+            this.btnAdaugareCurs.Click += new System.EventHandler(this.btnAdaugareCurs_Click);
+            // 
+            // btnAdaugareJob
+            // 
+            this.btnAdaugareJob.Location = new System.Drawing.Point(43, 510);
+            this.btnAdaugareJob.Name = "btnAdaugareJob";
+            this.btnAdaugareJob.Size = new System.Drawing.Size(141, 21);
+            this.btnAdaugareJob.TabIndex = 21;
+            this.btnAdaugareJob.Text = "Introduceti Job";
+            this.btnAdaugareJob.UseVisualStyleBackColor = true;
+            this.btnAdaugareJob.Click += new System.EventHandler(this.btnAdaugareJob_Click);
+            // 
+            // DataGrid
+            // 
+            this.DataGrid.AllowUserToDeleteRows = false;
+            this.DataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGrid.Location = new System.Drawing.Point(267, 35);
+            this.DataGrid.Name = "DataGrid";
+            this.DataGrid.RowTemplate.Height = 25;
+            this.DataGrid.Size = new System.Drawing.Size(398, 449);
+            this.DataGrid.TabIndex = 23;
+            // 
+            // btnAfisarePersoane
+            // 
+            this.btnAfisarePersoane.Location = new System.Drawing.Point(271, 514);
+            this.btnAfisarePersoane.Name = "btnAfisarePersoane";
+            this.btnAfisarePersoane.Size = new System.Drawing.Size(94, 49);
+            this.btnAfisarePersoane.TabIndex = 24;
+            this.btnAfisarePersoane.Text = "Afisare Persoane";
+            this.btnAfisarePersoane.UseVisualStyleBackColor = true;
+            this.btnAfisarePersoane.Click += new System.EventHandler(this.btnAfisarePersoane_Click);
+            // 
+            // btnAfisareCursuri
+            // 
+            this.btnAfisareCursuri.Location = new System.Drawing.Point(425, 514);
+            this.btnAfisareCursuri.Name = "btnAfisareCursuri";
+            this.btnAfisareCursuri.Size = new System.Drawing.Size(94, 49);
+            this.btnAfisareCursuri.TabIndex = 25;
+            this.btnAfisareCursuri.Text = "Afisare Cursuri";
+            this.btnAfisareCursuri.UseVisualStyleBackColor = true;
+            this.btnAfisareCursuri.Click += new System.EventHandler(this.btnAfisareCursuri_Click);
+            // 
+            // btnAfisareJoburi
+            // 
+            this.btnAfisareJoburi.Location = new System.Drawing.Point(571, 514);
+            this.btnAfisareJoburi.Name = "btnAfisareJoburi";
+            this.btnAfisareJoburi.Size = new System.Drawing.Size(94, 49);
+            this.btnAfisareJoburi.TabIndex = 26;
+            this.btnAfisareJoburi.Text = "Afisare Joburi";
+            this.btnAfisareJoburi.UseVisualStyleBackColor = true;
+            this.btnAfisareJoburi.Click += new System.EventHandler(this.btnAfisareJoburi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 588);
+            this.Controls.Add(this.btnAfisareJoburi);
+            this.Controls.Add(this.btnAfisareCursuri);
+            this.Controls.Add(this.btnAfisarePersoane);
+            this.Controls.Add(this.DataGrid);
+            this.Controls.Add(cbTipCurs);
+            this.Controls.Add(this.btnAdaugareJob);
+            this.Controls.Add(this.btnAdaugareCurs);
+            this.Controls.Add(this.btnAdaugarePersoana);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbFacultatePersoana);
+            this.Controls.Add(this.lbVarstaPersoana);
+            this.Controls.Add(this.lbNumePersoana);
+            this.Controls.Add(this.tbSalariuJob);
+            this.Controls.Add(this.tbNumeCurs);
+            this.Controls.Add(this.tbNumeJob);
+            this.Controls.Add(this.tbDepartamentJob);
+            this.Controls.Add(this.tbFacultatePersoana);
+            this.Controls.Add(this.tbVarstaPersoana);
+            this.Controls.Add(this.tbNumePersoana);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "App";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbNumePersoana;
+        private TextBox tbVarstaPersoana;
+        private TextBox tbFacultatePersoana;
+        private TextBox tbDepartamentJob;
+        private TextBox tbNumeJob;
+        private TextBox tbNumeCurs;
+        private TextBox tbSalariuJob;
+        private Label lbNumePersoana;
+        private Label lbVarstaPersoana;
+        private Label lbFacultatePersoana;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Button btnAdaugarePersoana;
+        private Button btnAdaugareCurs;
+        private Button btnAdaugareJob;
+        private ComboBox cbTipCurs;
+        private DataGridView DataGrid;
+        private Button btnAfisarePersoane;
+        private Button btnAfisareCursuri;
+        private Button btnAfisareJoburi;
+    }
+}
---
+++ Proiect_Resurse_Umane/Job.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_Resurse_Umane
+{
+    internal class Job : ICalculareComision
+    {
+        private string nume;
+        private string departament;
+        private float salariu;
+
+        public Job(string nume, string departament, float salariu)
+        {
+            this.nume = nume;
+            this.departament = departament;
+            this.salariu = salariu - CalculComision(salariu);
+        }
+
+        public static Job operator ++(Job obj)
+        {
+            obj.salariu += 100;
+            return obj;
+        }
+
+        public static Job operator --(Job obj)
+        {
+            obj.salariu -= 100;
+            return obj;
+        }
+
+        public float CalculComision(float salariu)
+        {
+            if (salariu < 2000) return 0;
+            else if (salariu >= 2000 && salariu < 5000) return salariu * 10 / 100;
+            else return salariu * 20 / 100;
+        }
+
+        public string Nume
+        {
+            get { return nume; }
+            set { nume = value; }
+        }
+        public string Departament
+        {
+            get { return departament; }
+            set { departament = value; }
+        }
+        public float Salariu
+        {
+            get { return salariu; }
+            set { salariu = value; }
+        }
+    }
+}
